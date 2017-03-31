d6d63df32fd484b23be1528d3a781cd5e808fc6b
kareleni <elenibritt@icloud.com>
2017-03-31 13:16:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-
+//hover on nav button one
+$("#onebutton").mouseenter(function(){
+    $("#onebutton").css("background-color" , "gold");
+    });
+$("#onebutton").mouseleave(function(){
+    $("#onebutton").css("background-color" , "white");
+    });
+    
+//hover on nav button two
+$("#twobutton").mouseenter(function(){
+    $("#twobutton").css("background-color" , "gold");
+    });
+$("#twobutton").mouseleave(function(){
+    $("#twobutton").css("background-color" , "white");
+    });
+    
+//hover on button three
+$("#threebutton").mouseenter(function(){
+    $("#threebutton").css("background-color" , "gold");
+    });
+$("#threebutton").mouseleave(function(){
+    $("#threebutton").css("background-color" , "white");
+    });
